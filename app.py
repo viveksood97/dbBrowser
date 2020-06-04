8b5e93ee37883327876191baa8ff87019edc8359
viveksood <sachin2.verma@airtel.com>
2020-06-04 20:47:59 +0530

    lots of changes including login/ register with encrypted passwords and clear clear filters

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, make_response,Markup,session,redirect, url_for, Response
+from flask import Flask, render_template, request, jsonify, make_response,Markup,session,redirect, url_for, Response,g
 from flask_restful import Resource, Api
 from flask import send_file
 import pandas as pd
@@ -9,29 +9,35 @@ import pymysql
 import time
 import json
 import math
+import sqlite3 as sql
+import os
+from passlib.hash import sha256_crypt
+from datetime import timedelta
 from io import BytesIO
 
 
 
-startTime = time.time()
+
 try:
     db_connection_str = 'mysql+pymysql://root:Airtel@123@localhost'
     db_connection = create_engine(db_connection_str)
     print("connection established")
 except:
     print("Error")
-endTime = time.time()
-# print(pd.read_sql('select * from CEN_db_mysql.cenCiscoXE_Interface limit 0,1;', con=db_connection ))
+
+
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "OCML3BRawWEUeaxcuKHLpw"
+app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=120)
 api = Api(app)
 
 
 
 def queryBuilder(database,tableName,word,columnName):
-    
+    usersession = session['user']
+    session.pop('user', None)
     paramDict = dict()
     # if(word):
     if word == "noword" and columnName == "nocolumn":
@@ -69,7 +75,7 @@ def queryBuilder(database,tableName,word,columnName):
     # else:
     #     string = f'select * from {database}.{tableName}'
     #     paramDict =  dict()
-    
+    session['user'] = usersession 
     return string, paramDict
 
 def lazy(query,shape,param="none"):
@@ -94,33 +100,60 @@ def lazy(query,shape,param="none"):
         value = "<p class='noEntry'><p>"
     return value
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    session.clear()
-    return render_template('root.html')
+    if request.method == 'POST':
+       
+        olmsid = request.form['username'].lower()
+        connection = db_connection.connect()
+        df1 = connection.execute("SELECT * FROM users.ingress WHERE olmid= %s", (olmsid)).fetchone()
+        if(df1):
+            if sha256_crypt.verify(request.form['password'], df1[5]):
+                connection.execute("UPDATE users.ingress SET visit = %s WHERE olmid= %s", (str(int(df1[6])+ 1),olmsid))
+                session['user'] = olmsid
+                session.permanent = True
+                return redirect(url_for('home'))
+            else:
+                return render_template('login.html',message="Incorrect Password")
+        else:
+           return render_template('login.html',message="Incorrect Username")
+           
+    return render_template('login.html')
+
+@app.route('/home')
+def home():
+    if g.user:
+        return render_template('root.html')
+
+    return redirect(url_for('index'))
+
+
 
 @app.route('/database')
 def database():
-    
+    usersession = session['user']
+    session.clear()
     if request.args:
         databaseName = request.args.get("c")
     tableNames =  pd.read_sql('show tables in '+databaseName, con=db_connection )
     tableNamesList = tableNames.to_json()
     res = make_response(tableNamesList, 200)
+    session['user'] = usersession
     return res
     
 @app.route('/tableHeaders')
 def tableHeaders():
+    usersession = session['user']
     session.clear()
     if request.args:
         mainData = request.args.get("c")
     data = mainData.split(",")
     database = data[0] 
     table = data[1]
-   
     headers =  pd.read_sql('select * from '+database+'.'+table+' limit 0,1;', con=db_connection )
     headers1 = list(headers.columns)
     res = make_response(json.dumps(headers1), 200)
+    session['user'] = usersession
     return res
 
 @app.route("/load")
@@ -134,7 +167,6 @@ def load():
     word = data[3]
     counter = int(data[4])
     query,params = queryBuilder(database,table,word,column)
-    # session["query"] = [query,params]
     if query:
         print(f"Returning posts {counter} to {counter + 35}")
         res = make_response(jsonify(lazy(query+' limit '+str(counter)+',35;',counter,params)), 200)
@@ -149,15 +181,13 @@ def downloadFile():
     if request.args:
         mainData = request.args.get("c")
     
-    # else:
     data = mainData.split(",")
     database = data[0] 
     table = data[1]
     column = data[2]
     word = data[3]
     query,params = queryBuilder(database,table,word,column)
-    # query = "select * from CEN_db_mysql.cenCiscoXE_Interface limit 0,1;"
-    # params = "none"
+    
     
     startTime = time.time()
     if(not query):
@@ -167,25 +197,17 @@ def downloadFile():
     else:
         df1 = pd.read_sql(query, con=db_connection )
 
-        # def generate():
-    #     for ele in x:
-    #        yield ','.join(ele.split())  + '\n'
-    # return Response(generate(), mimetype='text/csv')
     buffer = BytesIO()
     buffer.write(df1.to_csv().encode('utf-8'))
     buffer.seek(0)
 
     return send_file(buffer,mimetype='text/csv',attachment_filename='report.csv',as_attachment=True)
     
-
-
-    
-
 @app.route('/tableInfo')
 def tableInfo():
     if request.args:
         mainData = request.args.get("c")
-    # startTime = time.time()
+    
     
     data = mainData.split(",")
     database = data[0] 
@@ -193,25 +215,47 @@ def tableInfo():
     
     rows = pd.read_sql(f'select count(*) from {database}.{table}', con=db_connection)['count(*)'].iloc[0]
     size = pd.read_sql(f'SELECT table_name AS `Table`, round(((data_length + index_length) / 1024 / 1024)*4, 2) `Size in MB` FROM information_schema.TABLES WHERE table_schema = "{database}"    AND table_name = "{table}";', con=db_connection)['Size in MB'].iloc[0]
-    # endTime = time.time()
-    # print(endTime-startTime)
     return str(f"{rows}/{size}")
     
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user' in session:
+        g.user = session['user']
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        olmsid = request.form['username']
+        name = request.form['name']
+        department = request.form['department']
+        manager = request.form['manager']
+        password = request.form['password']
+        connection = db_connection.connect()
+        df1 = connection.execute("SELECT * FROM users.ingress WHERE olmid= %s", (olmsid)).fetchone()
+        hashedPassword = sha256_crypt.encrypt(password)
     
+        if(not df1):
+            connection.execute("INSERT INTO users.ingress (olmid,name,department,manager,password,permission,visit) VALUES(%s,%s,%s,%s,%s,%s,%s);",(olmsid,name,department,manager,hashedPassword,"default",0))
+            return render_template('register.html',message = "User Created")
+        else:
+            return render_template('register.html',message = "User Already Exists")
+
+    return render_template('register.html')
+
+    
+
+@app.route('/dropsession')
+def dropsession():
+    session.pop('user', None)
+    return redirect(url_for('index'))
+
 class fetch(Resource):
     def get(self, database, table):
-        df = pd.read_sql(f"select * from {database}.{table} limit 0,100", con=db_connection )
+        df = pd.read_sql(f"select * from {database}.{table}", con=db_connection )
         return df.to_json(orient='split')
 
 api.add_resource(fetch, '/fetch/<database>/<table>')
-        
-    
-    
-
-    #return Response(df1, mimetype='text/csv',headers={'Content-Disposition': f'attachment; filename={"report.csv"}'})
-    
-
-
 
 if __name__ == '__main__':
     app.run(use_reloader= True,debug=True,host='0.0.0.0', port=8011,threaded=True)
